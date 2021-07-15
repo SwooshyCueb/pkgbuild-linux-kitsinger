a4d061e710fdff086604be7efbfa78918b610f33
Markus Kitsinger (SwooshyCueb) <root@swooshalicio.us>
2021-07-15 00:09:01 -0400

    Add patch cleanup helper script

---
+++ clean-untracked-patches.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+shopt -s nullglob
+
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+patches_cur=(*.patch)
+if (( ${#patches_cur[@]} )); then
+	git clean -X *.patch
+else
+	echo "No patch files in current directory. Exiting."
+fi
