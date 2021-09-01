66d62f8fabb3317af8c410415465d520e20e20e0
Markus Kitsinger (SwooshyCueb) <root@swooshalicio.us>
2021-09-01 15:12:31 -0400

    something strange happened to the repo. anyway here's an update

--- clean-untracked-patches.sh
+++ clean-untracked-patches.sh
@@ -7,7 +7,7 @@ SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 patches_cur=(*.patch)
 if (( ${#patches_cur[@]} )); then
-	git clean -X *.patch
+	git clean -f -X *.patch
 else
 	echo "No patch files in current directory. Exiting."
 fi
